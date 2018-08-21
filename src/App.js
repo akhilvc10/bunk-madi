b174d5b52c74491205a36d6f92a7e4c3e15fc69e
Akhil <akhilvc10@gmail.com>
2018-08-22 01:28:43 +0530

    modified the function and fixed some bugs

--- src/App.js
+++ src/App.js
@@ -3,70 +3,84 @@ import "./App.css";
 
 class App extends Component {
 	state = {
-		attendancePercentage: " ",
-		classesToAttend: ""
+		attendencePercent: " ",
+		classesToAttend: "",
+		criteria: ""
 	};
 
-	attendedRef = React.createRef();
+	lecturesHeldRef = React.createRef();
 	bunkedRef = React.createRef();
 	marginRef = React.createRef();
 
 	getAttendance = event => {
 		event.preventDefault();
 		/*eslint-disable */
-		const attended = parseInt(this.attendedRef.current.value);
+
+		const lecturesHeld = parseInt(this.lecturesHeldRef.current.value);
 		const bunked = parseInt(this.bunkedRef.current.value);
-		const margin = parseInt(this.marginRef.current.value);
-		/*eslint-enable */
-		const attendancePercentage = parseFloat(
-			100 - (bunked / attended) * 100
-		).toFixed(2);
-		const classesToAttend = Math.ceil(
-			((attendancePercentage - margin) / 100) * attended
+		const criteria = parseInt(this.marginRef.current.value);
+		const lecturesAttended = parseInt(lecturesHeld - bunked);
+		const attendencePercent = parseFloat(
+			((lecturesAttended * 100) / lecturesHeld).toFixed(2)
 		);
+		const classesToAttend =
+			(criteria * lecturesHeld - lecturesAttended * 100) / (100 - criteria);
+		/*eslint-enable */
+
 		this.setState({
-			attendancePercentage,
-			classesToAttend
+			attendencePercent,
+			classesToAttend,
+			criteria
 		});
 	};
 
 	render() {
 		let content;
-		if (this.state.classesToAttend !== "") {
-			if (this.state.classesToAttend <= -1) {
+
+		if (this.state.classesToAttend > 0) {
+			if (
+				this.state.lecturesAttended === this.state.lecturesHeld ||
+				this.state.criteria !== 100
+			) {
 				content = (
 					<div className="message">
 						<div>
-							<i class="material-icons">warning</i>
+							<i className="material-icons">warning</i>
 						</div>
 						<span role="img" aria-label="sad">
 							😵
 						</span>{" "}
 						You have only{" "}
-						<b className="element"> {this.state.attendancePercentage}%</b> of
-						attendance and You have to attend{" "}
-						<b className="element">{this.state.classesToAttend * -1}</b> more
-						classes to maintain the min attendance.
+						<b className="element"> {this.state.attendencePercent}%</b> of
+						attendance and You should attend next{" "}
+						<b className="element">{this.state.classesToAttend + 1}</b> lectures
+						to maintain the min attendance.
 					</div>
 				);
-			} else if (this.state.classesToAttend > 0) {
-				content = (
-					<div className="message">
-						<span role="img" aria-label="happy">
-							😁
-						</span>{" "}
-						You have{" "}
-						<b className="element">{this.state.attendancePercentage}%</b> of
-						attendance. You can bunk{" "}
-						<b className="element">{this.state.classesToAttend}</b> more
-						classes. Enjoy !
-					</div>
-				);
-			} else if (this.state.classesToAttend === 0) {
-				content = <div className="message">You have 75%. Lucky AF !</div>;
-			} else {
-				return;
 			}
+		}
+		if (this.state.classesToAttend <= -1) {
+			const check = this.state.criteria / (100 - this.state.criteria);
+			/*eslint-disable */
+			const lectBunked = parseInt((-1 * this.state.classesToAttend) / check);
+			/*eslint-enable */
+			content = (
+				<div className="message">
+					<span role="img" aria-label="happy">
+						😁
+					</span>{" "}
+					You have <b className="element">{this.state.attendencePercent}%</b> of
+					attendance. You can bunk <b className="element">{lectBunked}</b> more
+					lectures. Enjoy !
+				</div>
+			);
+		}
+		if (this.state.classesToAttend === 0) {
+			content = (
+				<div className="message">
+					You have {this.state.attendencePercent}% of Attendace. Lucky AF !
+				</div>
+			);
 		}
 
 		return (
@@ -75,13 +89,13 @@ class App extends Component {
 					<h1>Bunk Māḍi !</h1>
 
 					<input
-						placeholder="Total Lectures"
+						placeholder="Total Lectures Held"
 						type="number"
 						pattern="[0-9]*"
 						inputMode="numeric"
 						required
-						ref={this.attendedRef}
-						name="attended"
+						ref={this.lecturesHeldRef}
+						name="lecturesheld"
 					/>
 					<input
 						placeholder="Bunked Lectures"
